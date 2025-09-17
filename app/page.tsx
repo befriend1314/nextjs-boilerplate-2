3ae832f81579034e7d8dabb019668eb5f90ffa40
befriend1314 <769970587@qq.com>
2025-09-17 17:25:44 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,12 @@ export default function Home() {
           height={38}
           priority
         />
+        <p>婷婷你好</p>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
+              app/page.tsx 
             </code>
             .
           </li>
